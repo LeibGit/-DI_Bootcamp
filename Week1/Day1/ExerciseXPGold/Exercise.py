507102304dec55aaa35c798e9472a99d52b1fad9
Leib <leibnroth@gmail.com>
2025-11-02 16:40:18 +0200

    completed gold exercises

---
+++ Week1/Day1/ExerciseXPGold/Exercise.py
@@ -0,0 +1,10 @@
+import calendar
+
+
+# Exercise 1 : Hello World-I love Python
+print(("Hello World \n" * 4 + "I Love Python \n" * 4))
+
+# Exercise 2: What Is The Season?
+import calendar
+prompt = int(input("Input a month 1-12: "))
+print(calendar.month_name[prompt])

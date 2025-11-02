4e684703765193433f1bf0f4c71b2d076430b319
Leib <leibnroth@gmail.com>
2025-11-02 15:54:22 +0200

    completed excerise.py

---
+++ Week1/Day1/ExxerciseXP/Exercise.py
@@ -0,0 +1,44 @@
+# Excercise 1
+string = 'Hello World\n'
+print(string * 4 )
+
+# Excercise 2
+result = ((99**3)*8)
+print(result)
+
+# Excercise 3
+# 5 < 3 False
+# 3 == 3 True
+# 3 == '3' False
+# "3" > 3 False
+# "Hello" == "hello" False
+
+# Excercise 4
+computer_brand = 'Lenovo'
+print(f"I have a {computer_brand} computer.")
+
+# Excercise 5
+name = 'Leib' 
+age = 21
+shoe_size = 12
+info = f"My name is {name}. I am {age} years old, and my shoe size is {shoe_size}"
+print(info)
+
+# Exercise 6: A & B
+a = 52
+b = 8
+if a > b:
+  print("Hello Wolrd")
+
+# Excercise 7: Odd or Even
+prompt = int(input("Enter a number: "))
+if prompt % 2 == 0:
+  print('Even')
+else: 
+  print('Odd')
+
+# Excercise 8: What is your name?
+prompt = str(input("What is your name: ")).lower()
+if prompt == 'leib':
+  print(f"Your name is {prompt} and my name is leib. we have the same name")
+

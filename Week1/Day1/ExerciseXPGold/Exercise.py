795b8d867117bfe69e6cef7eb684333f9d0d8018
Leib <leibnroth@gmail.com>
2025-11-02 19:25:42 +0200

    completed exercise 2-4 in shell

--- Week1/Day1/ExerciseXPGold/Exercise.py
+++ Week1/Day1/ExerciseXPGold/Exercise.py
@@ -16,4 +16,19 @@ elif prompt >= 9 and prompt <= 11:
   print("Autumn runs from September (9) to November (11)")
 else:
   print("Winter runs from December (12) to February (2)")
-  
+
+# Exercise 4
+my_text = """Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+           sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+           Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+           laboris nisi ut aliquip ex ea commodo consequat. 
+           Duis aute irure dolor in reprehenderit in voluptate velit 
+           esse cillum dolore eu fugiat nulla pariatur. 
+           Excepteur sint occaecat cupidatat non proident, 
+           sunt in culpa qui officia deserunt mollit anim id est laborum."""
+print(len(my_text))
+
+# Exercise 5
+prompt = input("Input the longest sentence you can without the letter A: ").lower()
+
+(prompt.split())

3ece0b38004ff212c3da9b06e204793cdc5c15ad
Leib <leibnroth@gmail.com>
2025-11-09 08:19:37 +0200

    updated day 5 challenges

--- Week1/mini_project/challanges.py
+++ Week1/mini_project/challanges.py
@@ -66,14 +66,68 @@ def norm(nums: list):
     print(l2_norm)
 norm([1, 2, 2])
 
-def is_monotonic(nums: list):
-    for index, num in enumerate(nums):
-        if num[index] < num[index + 1]:
-            print('ascending')
-            return True
-        elif num[index] > num[index + 1]:
-            print('decending')
-            return True
+
+def longest_word(words: list):
+    length = []
+    for word in words:
+        length.append(len(word))
+    index_max_str =  length.index(max(length)) 
+    print(words[index_max_str])
+longest_word(['hello', 'jello', 'spy', 'coding'])
+
+def seperate(items: list):
+    strings = []
+    nums = []
+    for item in items:
+        if type(item) == int:
+            nums.append(item)
         else:
-            return False
-is_monotonic([7,6,5,5,2,0])
+            strings.append(item)
+    print(strings)
+    print(nums)
+seperate([1, 'hello', 12, 'spy', 56, 'code'])
+
+def is_palindrome(text: str):
+    if text == text[::-1]:
+        print("is palindrome")
+    else:
+        print("Not palindrome")
+is_palindrome('john')
+is_palindrome('radar')
+
+def sum_over_k(sentence: str, k: int):
+    greater_than = []
+    words = sentence.split()
+    for word in words:
+        if len(word) > k:
+            greater_than.append(word)
+    print(greater_than)
+sum_over_k(sentence='Do or do not there is no try', k=3)
+
+def average(items: dict):
+    lst_of_values = []
+    total = 0
+    for value in items.values():
+        lst_of_values.append(value)
+        total += value
+    
+    avg = total / len(lst_of_values)
+    print(avg)
+average({'a': 1,'b':2,'c':8,'d': 1})
+
+def common_div(n1, n2):
+    list_of_div = []
+    get_min =  min(n1, n2)
+    numbers = list(range(2, get_min + 1))
+    print(numbers)
+    for n in numbers:
+        if n1 % n == 0 and n2 % n == 0:
+            list_of_div.append(n)
+    print(list_of_div)
+common_div(10, 20)
+
+def weird_print(lst: list):
+    for index, value in enumerate(lst):
+        if value % 2 == 0:
+            print(f"{index}: {value}")
+weird_print([1,2,2,3,4,5])

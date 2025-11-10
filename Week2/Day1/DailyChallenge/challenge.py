17c56dd749b2b4f94207641d69750c58a7d13871
Leib <leibnroth@gmail.com>
2025-11-10 10:49:54 +0200

    challenge complete

---
+++ Week2/Day1/DailyChallenge/challenge.py
@@ -0,0 +1,22 @@
+class Farm():
+    def __init__(self, name: str, animals: dict):
+        self.name = name
+        self.animals = animals
+
+    def add_animal(self, animal_type, count=1):
+        for animal, c in self.animals.items():
+            if animal in self.animals.keys():
+                self.animals[animal] = c + count
+            else:
+                self.animals[animal] = count
+        print(self.animals)
+
+    def get_info(self):
+        print(f"{self.name}:")
+        for animal, count in self.animals.items():
+            print(f"{animal}: {count}")
+        print("E-I-E-I-O")
+
+some_farm = Farm("Some Farm", {'cow': 1, 'pig':3, 'horse': 2})
+some_farm.add_animal("cow", 5)
+some_farm.get_info() 
---
+++ Week2/Day1/ExerciseXP/menu_manager.py
@@ -0,0 +1,11 @@
+class MenuManager():
+    def __init__(self, menu: list):
+        self.menu = menu
+
+    def add_item():
+        
+current_menu = [
+    {"name": "soup", "price": 10, "spice": "B", "gluten_index": False}
+]
+random_menu = MenuManager()
+random_menu.add_item()

13c48ac8abc7c4c3bce3f9da07cb6c28df2f3381
Leib <leibnroth@gmail.com>
2025-11-11 11:33:27 +0200

    exercise 3 done

---
+++ Week2/Day2/ExerciseXP/exercise3.py
@@ -0,0 +1,40 @@
+from exercise import Dog
+import random
+
+class PetDog(Dog):
+    def __init__(self, name, age, weight, trained=False):
+        super().__init__(name, age, weight)
+
+    def train(self):
+        self.bark()
+        self.trained = True
+        print(self.trained)
+    
+    def play(self, *dogs):
+        dog_names = [dog.name for dog in dogs]
+        try:
+            print(f"{', '.join(dog_names)} all play together.")
+        except Exception as e:
+            print(e)
+
+    def do_a_trick(self):
+        if self.trained == True:
+            try:
+                trick = random.choice(tricks)
+                print(f"{self.name} {trick}")
+            except Exception as e:
+                print(e)
+
+tricks = ["does a barrel roll", "stands on his back legs", "shakes your hand", "plays dead"]
+
+dog1 = PetDog("Will", 12, 120)
+dog2 = PetDog("Marie", 15, 190)
+dog3 = PetDog("Shawn", 25, 130)
+
+dog1.train()
+dog1.play(dog2, dog2)
+dog1.do_a_trick()
+
+dog2.train()
+dog2.play(dog1, dog3)
+dog2.do_a_trick()

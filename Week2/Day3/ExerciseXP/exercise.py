8dab70700b4216e1d4e693e2984e37076b0a0527
Leib <leibnroth@gmail.com>
2025-11-12 21:43:13 +0200

    finished exercise one

--- Week2/Day3/ExerciseXP/exercise.py
+++ Week2/Day3/ExerciseXP/exercise.py
@@ -1,3 +1,5 @@
+# Exercise 1
+
 class Currency:
     def __init__(self, currency, amount):
         self.currency = currency
@@ -33,21 +35,61 @@ class Currency:
             return self
         else:
             raise ValueError("Must be an instance of a currency")
+        
+# Exercise 2
+from func import sum_of_nums
 
-if __name__=='__main__':
-    c1 = Currency('dollar', 5)
-    c2 = Currency('dollar', 10)
-    c3 = Currency('shekel', 1)
-    c4 = Currency('shekel', 10)
+# Exercise 3
+import string
+import random
 
-    print(c1)
-    print(int(c1))
-    print(repr(c1))
-    print(c1 + 5)
-    print(c1 + c2)
-    print(c1)
-    c1 += 5
-    print(c1)
-    c1 += c2
-    print(c1)
-    print(c1 + c3)
+upper_alphabet = string.ascii_uppercase
+
+new_str = ''
+for i in range(0, 5):
+    rand = random.choice(upper_alphabet)
+    new_str += rand
+
+# Exercise 4
+
+from datetime import datetime, date
+
+current_time= datetime.now()
+current_date = current_time.date()
+
+# Exercise 5
+
+january_first = date(2025, 1, 1)
+print(january_first)
+
+def time_till():
+    return current_date - january_first
+
+# Exercise 6
+birthday = date(2004, 10, 12)
+def time_alive():
+    delta = current_date - birthday
+    total_minutes = delta.total_seconds() / 60
+    return total_minutes
+
+# Exercise 7
+from faker import Faker
+
+fake = Faker()
+users = []
+
+def add_users(users_to_generate):
+    for user in range(users_to_generate):
+        user_dict = {"name": fake.name(), "address": fake.address(), "language_code": fake.language_code()}
+        users.append(user_dict)
+    return users
+
+
+
+if __name__=="__main__":
+    sum_of_nums(12, 5)
+    print(new_str)
+    print(current_date)
+    print(time_till())
+    print(time_alive())
+    print(add_users(12))

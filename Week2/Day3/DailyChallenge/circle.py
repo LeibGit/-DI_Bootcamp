6add4b2aeccc0449bf9e8d9a8d4cdfcc0dcbf7df
Leib <leibnroth@gmail.com>
2025-11-13 11:21:50 +0200

    finished daily challenge

--- Week2/Day3/DailyChallenge/circle.py
+++ Week2/Day3/DailyChallenge/circle.py
@@ -1,4 +1,5 @@
-from math import pi
+from math import pi, sqrt
+import turtle
 
 class Circle():
     def __init__(self, radius=None, diameter=None):
@@ -15,7 +16,53 @@ class Circle():
         else:
             area = 1/4*pi*self.diameter**2
             return area
+
+    def __str__(self):
+        return self.diameter, self.radius
     
+    def __add__(self, other):
+        if isinstance(other, Circle):
+            total_area = self.compute_area() + other.compute_area()
+            radius = sqrt(total_area/pi)
+            return radius
+        else:
+            raise ValueError("Other circle added is not an instance.")
+        
+    def __gt__(self, other):
+        if isinstance(other, Circle):
+            if self.compute_area() > other.compute_area():
+                return f"{self.compute_area()} is greater than {other.compute_area()}"
+            elif self.compute_area() < other.compute_area():
+                return f"{self.compute_area()} is less than {other.compute_area()}"
+            else:
+                return "They have the same area."
+            
+    def __eq__(self, other):
+        if isinstance(other, Circle):
+            if self.compute_area() == other.compute_area():
+                return "They are equal."
+            else:
+                return "they are not equal."
+        
+    
+    def __lt__(self, lst):
+        return self.compute_area() < lst.compute_area()
+
+    def draw_cirle(self):
+        return turtle.circle(self.compute_area())
+    
+        
 if __name__=="__main__":
+
     circle1 = Circle(diameter=10)
+    circle2 = Circle(radius=10)
+    circle3 = Circle(radius=10)
+    circle4 = Circle(diameter=24)
+    list_of_circles = [circle1, circle2, circle3, circle4]
     print(circle1.compute_area())
+    print(circle1.__str__())
+    print(circle1.__add__(circle2))
+    print(circle1.__gt__(circle2))
+    print(circle1.__eq__(circle2))
+    print(sorted(list_of_circles))
+    print(circle3.draw_cirle())

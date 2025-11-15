6c1e108ca9eafcb9e5dd544275d58e63f346abc0
Leib <leibnroth@gmail.com>
2025-11-15 13:02:56 +0200

    dailychallenge done

--- Week2/Day4/DailyChallenge/challenge.py
+++ Week2/Day4/DailyChallenge/challenge.py
@@ -1,3 +1,9 @@
+import string
+import nltk
+from nltk.corpus import stopwords
+import re
+
+nltk.download('stopwords')
 class Text():
     def __init__(self, text: str):
         self.text = text
@@ -33,13 +39,44 @@ class Text():
                 word_set.add(word)
         return list(word_set)
     
+    def form_file(self, file_path):
+        with open(file_path, 'r') as file:
+            content = file.read()
+        return content
+
 class TextModification(Text):
-    def __init__(self, data):
-        super().__init__(data)
-        pass
+    def __init__(self, text):
+        super().__init__(text)
+    
+    def remove_punctuation(self):
+        punctuation_chars = string.punctuation
+        print(punctuation_chars)
+        for char in punctuation_chars:
+            if char in self.text:
+                new_str = self.text.replace(f'{char}', '')
+        return new_str
+
+    def remove_stop_words(self):
+        stop_words = list(stopwords.words('english'))
+        for word in stop_words:
+            if word in self.text:
+                new_text = self.text.replace(f'{word}', '')
+        return new_text
+    
+    def remove_special_chars(self):
+        special_chars = re.findall(r'[^a-zA-Z0-9\s]', self.text)
+        for char in special_chars:
+            if char in self.text:
+                new_text = self.text.replace(f'{char}', '')
+        return new_text
 
 if __name__=="__main__":
     test =  Text("Hello This Is Hello.")
+    test2 = TextModification("Hello This Is Hello.")
     print(test.word_frequency("Hello"))
     print(test.most_common_word())
     print(test.unique_words())
+    print(test.form_file(r"C:\Users\leibn\Downloads\random_words.txt"))
+    print(test2.remove_punctuation())
+    print(test2.remove_stop_words())
+    print(test2.remove_special_chars())

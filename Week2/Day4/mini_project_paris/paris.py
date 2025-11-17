47017ffae1dcf1c55ea616509884638e9be01723
Leib <leibnroth@gmail.com>
2025-11-17 14:13:28 +0200

    increased readability

--- Week2/Day4/mini_project_paris/paris.py
+++ Week2/Day4/mini_project_paris/paris.py
@@ -1,9 +1,17 @@
 from pyowm.owm import OWM
 owm = OWM('e57d4cad9f8a3f0cc066f010748dbccf')
 mgr = owm.weather_manager()
-observation = mgr.weather_at_place('Paris,FR')  # the observation object is a box containing a weather object
-weather = observation.weather
-weather.detailed_status
+
+def weather_info():
+    user_location = int(input("Enter the location id you wish to search: "))
+    observation = mgr.weather_at_id(user_location)  # the observation object is a box containing a weather object
+    weather = observation.weather
+    return weather
 
 if __name__=="__main__":
-    print(weather.detailed_status)
+    sf = weather_info()
+    print(f"{sf.detailed_status} is the weather status.")
+    wi = sf.wind()
+    print(f"{wi['speed']} meters per second")
+    print(f"Sunrise Time: {sf.sunrise_time(timeformat='iso')}")
+    print(f"Sunset Time: {sf.sunset_time(timeformat='iso')}")
